c18eb51688bbcd6522c91266e91ff694e45323a5
brijeshsajeev <brijesh00300@gmail.com>
2024-06-24 11:51:26 +0530

    Add shared examples for numeric operations in RSpec
    - Defined shared examples to test numeric operations
      - Type checking for first and second arguments

---
+++ spec/support/shared_examples_for_numeric_operations.rb
@@ -0,0 +1,9 @@
+RSpec.shared_examples "a numeric operation function" do
+  it "raises TypeError when first argument is not a number" do
+    expect { subject.call("a", 3) }.to raise_error(TypeError, "Please enter a number")
+  end
+
+  it "raises TypeError when second argument is not a number" do
+    expect { subject.call(2, "b") }.to raise_error(TypeError, "Please enter a number")
+  end
+end
