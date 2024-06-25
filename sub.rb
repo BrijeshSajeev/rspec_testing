60ab4df85533676bf4b1bf176aa4448b6dc9fbbc
brijeshsajeev <brijesh00300@gmail.com>
2024-06-25 20:47:00 +0530

    Adjust the indendation

--- sub.rb
+++ sub.rb
@@ -1,4 +1,9 @@
 def sub(num1, num2)
+
+  return num2 if num1.nil?
+  return num1 if num2.nil?
+
+
   unless num1.is_a?(Numeric) && num2.is_a?(Numeric)
     raise TypeError, "Please enter a number"
   end
