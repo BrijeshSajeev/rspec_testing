8d187784ba61d498a3b450a5468d869ce7075260
brijeshsajeev <brijesh00300@gmail.com>
2024-06-25 20:46:20 +0530

    Adjust the indendation

--- add.rb
+++ add.rb
@@ -1,6 +1,11 @@
 def add(num1, num2)
+
+  return num2 if num1.nil?
+  return num1 if num2.nil?
+
   unless num1.is_a?(Numeric) && num2.is_a?(Numeric)
     raise TypeError, "Please enter a number"
   end
+
   num1+num2
 end
